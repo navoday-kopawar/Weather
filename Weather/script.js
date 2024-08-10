89ba11fca6e286e9141e501c08998d9f5678a156
Navoday Kopawar <navodaykopawar@gmail.com>
2024-08-10 13:58:01 +0530

    Add files via upload

---
+++ Weather/script.js
@@ -0,0 +1,49 @@
+function getWeather() {
+    const apiKey = '1dbfb59e74edd516aaef4077e6712cfb';
+    const city = document.getElementById('city').value.trim();
+
+    if (!city) {
+        alert('Please enter a city');
+        return;
+    }
+
+    const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(currentWeatherUrl)
+        .then(response => response.json())
+        .then(data => displayWeather(data))
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+            alert('Error fetching weather data. Please try again.');
+        });
+}
+
+function displayWeather(data) {
+    const tempDivInfo = document.getElementById('temp-div');
+    const weatherInfoDiv = document.getElementById('weather-info');
+    const weatherIcon = document.getElementById('weather-icon');
+
+    tempDivInfo.innerHTML = '';
+    weatherInfoDiv.innerHTML = '';
+    weatherIcon.style.display = 'none';
+
+    if (data.cod !== 200) {
+        weatherInfoDiv.innerHTML = `<p>${data.message}</p>`;
+        return;
+    }
+
+    const cityName = data.name;
+    const temperature = Math.round(data.main.temp); 
+    const description = data.weather[0].description;
+    const iconCode = data.weather[0].icon;
+    const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
+
+    const temperatureHTML = `<p>${temperature}°C</p>`;
+    const weatherHtml = `<p>${cityName}</p><p>${description}</p>`;
+
+    tempDivInfo.innerHTML = temperatureHTML;
+    weatherInfoDiv.innerHTML = weatherHtml;
+    weatherIcon.src = iconUrl;
+    weatherIcon.alt = description;
+    weatherIcon.style.display = 'block'; 
+}
